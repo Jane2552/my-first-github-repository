13329f7a6b0c6bd49a1f837b2d662b9c615400d9
Jane <evelyn56123@gmail.com>
2025-10-23 18:51:00 +0800

    提交

--- while.py
+++ while.py
@@ -1,2 +1,10 @@
-total_sum = sum(range(1, 101))
-print(total_sum)
+user_input = int(input("請輸入任意整數: "))
+user_input_cal = []
+
+while user_input != 0:
+    user_input_cal.append(user_input)
+    user_input = int(input("請輸入任意整數: "))
+
+accumlation = sum(user_input_cal)
+
+print("目前累加的總數為: ", accumlation)
